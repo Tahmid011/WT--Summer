30953aedcbd5d82b4738983e77bc0eeeba1266c2
Tahmid <biswastahmidul@gmail.com>
2025-07-26 03:41:30 +0600

    fix

--- Mid/Lab Task 1/task1.php
+++ Mid/Lab Task 1/task1.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
     <body>
-        <H1>AIUB</H1>
-        <h3>Registration Form</h3>
-        <h3>Complete the registration form</h3>
+        <center>
+            <H1 style="color:blue">AIUB</H1>
+            <h3 style="color:blue">Registration Form</h3>
+        </center>
+
+        <h3>Complete the Registration</h3>
 
         <table>
             <tr>
@@ -29,16 +32,16 @@
             <tr>
                 <td>Language known:</td>
                 <td>
-                    <input type="checkbox">Bangla
                     <input type="checkbox">English
+                    <input type="checkbox">Bangla
                     <input type="checkbox">Hindi
                 </td>
             </tr>
             <tr>
-                <td>Country Select:</td>
+                <td>Country:</td>
                 <td>
                     <Select >
-                        <option>Select</option>
+                        <option>--Select--</option>
                         <option>America</option>
                         <option>Bangladesh</option>
                         <option>Canada</option>
@@ -54,7 +57,7 @@
                 <td><input type="file"></td>
             </tr>
             <tr>
-                <td>Comment:</td>
+                <td>Comments:</td>
                 <td><textarea cols="30" rows="5"></textarea></td>
             </tr>
             <tr>
