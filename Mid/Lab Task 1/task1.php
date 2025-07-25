57c5677748da783110cc41ef8a5bce561b8623ac
Tahmid <biswastahmidul@gmail.com>
2025-07-26 03:22:48 +0600

    add task

--- Mid/Lab Task 1/task1.php
+++ Mid/Lab Task 1/task1.php
@@ -1,1 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <H1>AIUB</H1>
+        <h3>Registration Form</h3>
+        <h3>Complete the registration form</h3>
 
+        <table>
+            <tr>
+                <td>Full Name:</td>
+                <td><input type ="text"></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type = "email"></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password"></td>
+            </tr>
+            <tr>
+                <td>Gender:</td>
+                <td>
+                    <input type="radio" name="gender" value="male">Male
+                    <input type="radio" name="gender" value="others">Others
+            
+                </td>
+            </tr>
+            <tr>
+                <td>Language known:</td>
+                <td>
+                    <input type="checkbox">Bangla
+                    <input type="checkbox">English
+                    <input type="checkbox">Hindi
+                </td>
+            </tr>
+            <tr>
+                <td>Country Select:</td>
+                <td>
+                    <Select >
+                        <option>Select</option>
+                        <option>America</option>
+                        <option>Bangladesh</option>
+                        <option>Canada</option>
+                    </Select>
+                </td>
+            </tr>
+            <tr>
+                <td>Date of Birth</td>
+                <td><input type="date"></td>
+            </tr>
+            <tr>
+                <td>Upload Photo:</td>
+                <td><input type="file"></td>
+            </tr>
+            <tr>
+                <td>Comment:</td>
+                <td><textarea cols="30" rows="5"></textarea></td>
+            </tr>
+            <tr>
+                <td>Register</td>
+                <td><input type="button" value="Register"></td>
+            </tr>
+        </table>
+
+
+    </body>
+</html>
